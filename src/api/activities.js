878b7c2fac4aae47434471827fe60bff5d2dade3
Hayley McVicar <hrmcvicar@gmail.com>
2025-11-13 20:44:06 -0800

    I tried

--- src/api/activities.js
+++ src/api/activities.js
@@ -35,3 +35,22 @@ export async function createActivity(token, activity) {
     throw Error(result.message);
   }
 }
+
+//delete function
+//token
+
+export async function deleteActivity(token, id) {
+  if (!token) {
+    throw Error("You must be signed in to delete an activity.");
+  }
+
+  const response = await fetch(API + "/activities/" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
